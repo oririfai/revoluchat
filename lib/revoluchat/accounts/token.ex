a43c129631bff1c9e8c076b69cb0edb1ca172550
oririfai <oririfai@gmail.com>
2026-07-08 15:48:28 +0700

    [DEBUG]fallback no kid in token

--- lib/revoluchat/accounts/token.ex
+++ lib/revoluchat/accounts/token.ex
@@ -29,6 +29,21 @@ defmodule Revoluchat.Accounts.Token do
       {:ok, _claims} ->
         {:error, :missing_sub_claim}
 
+      {:error, :no_kid_in_token_header} ->
+        require Logger
+        Logger.warning("verify_access_token: Token missing kid. Attempting to verify with all available signers...")
+        
+        # Coba fallback manual tanpa mengecek kid (coba semua kunci publik yang ada)
+        case fallback_verify_without_kid(token_string) do
+          {:ok, %{"sub" => sub} = claims} ->
+            user_id = parse_user_id(sub)
+            app_id = Map.get(claims, "app_id")
+            {:ok, %{user_id: user_id, app_id: app_id}}
+
+          {:error, reason} ->
+            {:error, reason}
+        end
+
       {:error, :no_signers_fetched} ->
         require Logger
         Logger.warning("verify_access_token: No signers fetched. Attempting synchronous refresh of JWKS keys...")
@@ -59,4 +74,31 @@ defmodule Revoluchat.Accounts.Token do
   defp parse_user_id(sub) when is_float(sub), do: trunc(sub) |> to_string()
   defp parse_user_id(sub) when is_integer(sub), do: to_string(sub)
   defp parse_user_id(sub) when is_binary(sub), do: sub
+
+  defp fallback_verify_without_kid(token_string) do
+    case Revoluchat.Accounts.JwksStrategy.list_signers() do
+      {:ok, signers} when is_map(signers) and map_size(signers) > 0 ->
+        # Coba verifikasi token dengan setiap public key (signer) yang ada
+        Enum.reduce_while(Map.values(signers), {:error, :signature_error}, fn signer, _acc ->
+          case Revoluchat.Accounts.Token.Fallback.verify_and_validate(token_string, signer) do
+            {:ok, claims} -> {:halt, {:ok, claims}}
+            {:error, reason} -> {:cont, {:error, reason}}
+          end
+        end)
+
+      _ ->
+        {:error, :no_signers_fetched}
+    end
+  end
+end
+
+defmodule Revoluchat.Accounts.Token.Fallback do
+  @moduledoc false
+  use Joken.Config
+
+  @impl true
+  def token_config do
+    # Konfigurasi sama dengan modul utama, tapi tanpa hook JokenJwks
+    default_claims(skip: [:aud, :iss])
+  end
 end
